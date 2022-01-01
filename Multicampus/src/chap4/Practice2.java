69fa555205990dc11abcaf4eede7faadbc364160
seoltaehyeok <ajaj3122@naver.com>
2022-01-01 13:31:53 +0900

    자바의정석 연습문제4-2

---
+++ Multicampus/src/chap4/Practice2.java
@@ -0,0 +1,15 @@
+package chap4;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+		for(int i=1; i<=20; i++) {
+			if(i%2==0 || i%3==0)
+				continue;
+			sum +=i;
+		}
+		System.out.println(sum);
+	}
+}
