b6e091f8a2d48f49f23636cdb7f76cc02a4cfef5
taehyeok <ajaj3122@naver.com>
2022-01-05 22:32:02 +0900

    Calendar logic4

---
+++ Multicampus/src/study/CalendarService4.java
@@ -0,0 +1,66 @@
+package study;
+
+import java.util.Scanner;
+
+public class CalendarService4 {
+
+	public static void main(String[] args) {
+		CalendarService4 cm = new CalendarService4();
+		cm.start();
+	}
+
+	public void start() {
+		Scanner scan = new Scanner(System.in);
+		boolean flag = false;
+		
+		while (!flag) {
+			System.out.print("input month please >>> ");
+			int cmd = scan.nextInt();
+
+			makeCalendar(cmd);
+
+			System.out.print("be continue? [Yes:1, No:0] >>> ");
+			cmd = scan.nextInt();
+			if (cmd == 0) {
+				System.out.println("Calendar Service is Stop!!");
+				flag = true;
+			}
+		}
+		scan.close();
+	}
+
+	void makeCalendar(int mon) {
+		int year = 2021;
+		int month = mon;
+		int[] spaces = new int[12];
+		int[] lastDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		spaces[0] = (2 + 31) % 7;
+		for (int i = 1; i < 12; i++) {
+			spaces[i] = (spaces[i - 1] + lastDays[i - 1]) % 7;
+		}
+
+		int space = spaces[month - 1];
+		int lastDay = lastDays[month - 1];
+		
+		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+			lastDay = 29;
+		}
+
+		System.out.printf("%d년 %d월 %n", year, month);
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		for (int i = 0; i < space; i++) {
+			System.out.print("\t");
+		}
+		int day = 1;
+		for (int i = 0; i < lastDay; i++) {
+			System.out.print(day + "\t");
+			if ((space + day) % 7 == 0) {
+				System.out.println();
+			}
+			day = day + 1;
+		}
+		System.out.println("\n");
+	}
+
+}
